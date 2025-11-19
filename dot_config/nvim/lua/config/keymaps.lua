1e37ad20e84c96524c412f7f5da5eddbd3aa73a1
LeSurvivant9 <mcalexandreboisfer@gmail.com>
2025-11-19 18:37:31 +0100

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yank" })
+vim.keymap.set({ "n", "v" }, "dd", '"_dd', { desc = "Delete line without yank" })
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete char without yank" })
+vim.keymap.set("x", "p", '"_dP', { desc = "Paste without overwriting" })
